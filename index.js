5f09b347551dad57e39cf241eeea26b7c4303839
Amy <86963506+aeposten@users.noreply.github.com>
2022-10-20 16:49:08 -0700

    Initial functions written

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-let pottyCounter= document.getElementById("potty-count");
-let lunchCounter= document.getElementById("lunch-count")
-let bedCounter= document.getElementById("bed-count");
-
+let pottyCounter = document.getElementById("potty-count");
+let lunchCounter = document.getElementById("lunch-count");
+let bedCounter = document.getElementById("bed-count");
+let feedCounter = document.getElementById("feed-count");
+let readCounter = document.getElementById("read-count");
+let totalCounter = document.getElementById("total-count");
+let total = 0;
 let count;
 
-// function incrementLunch() {
-//     lunchCount += 1
-//     lunchCounter.textContent = lunchCount;
-// }
-
-function selectComponent (elementId) {
-    let component = document.getElementById(elementId);
-    return component;
+function selectComponent(elementId) {
+  let component = document.getElementById(elementId);
+  return component;
 }
 
-function increment (elementId) {
-    count =  parseInt(selectComponent(elementId).textContent);
-    count += 1
-    selectComponent(elementId).textContent = count;
-   console.log(count)
+function increment(elementId) {
+  count = parseInt(selectComponent(elementId).textContent);
+  count += 1;
+  selectComponent(elementId).textContent = count;
 }
 
-function reset (elementId) {
-    
+function reset(elementId) {
+  count = 0;
+  selectComponent(elementId).textContent = count;
+}
+
+function updateTotal() {
+  total =
+    parseInt(pottyCounter.textContent) +
+    parseInt(lunchCounter.textContent) +
+    parseInt(bedCounter.textContent) +
+    parseInt(feedCounter.textContent) +
+    parseInt(readCounter.textContent);
+  totalCounter.textContent = total;
 }
