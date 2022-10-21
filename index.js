aaa8427f3f0bb2dcad0af3b820d39f7a13dce680
Amy <86963506+aeposten@users.noreply.github.com>
2022-10-21 14:24:28 -0700

    Refactoring

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-let pottyCounter = document.getElementById("potty-count");
-let lunchCounter = document.getElementById("lunch-count");
-let bedCounter = document.getElementById("bed-count");
-let feedCounter = document.getElementById("feed-count");
-let readCounter = document.getElementById("read-count");
-let totalCounter = document.getElementById("total-count");
+//Global variables for chore chart
+let pottyCounter = selectComponent("potty-count");
+let lunchCounter = selectComponent("lunch-count");
+let bedCounter = selectComponent("bed-count");
+let feedCounter = selectComponent("feed-count");
+let readCounter = selectComponent("read-count");
+let totalCounter = selectComponent("total-count");
 let total = 0;
 let count;
 
+//Global variables for timer
 let timer;
-let timerSpan = document.getElementById("time");
+let timerSpan = selectComponent("time");
 let initialMinutes = 20;
 let totalSeconds = initialMinutes * 60;
 let paused = true;
 
+
+//Gets component by id
 function selectComponent(elementId) {
   let component = document.getElementById(elementId);
   return component;
 }
 
+//Increments individual chore counter
 function increment(elementId) {
   count = parseInt(selectComponent(elementId).textContent);
   count += 1;
   selectComponent(elementId).textContent = count;
 }
 
+//Resets individual chore counter
 function reset(elementId) {
   count = 0;
   selectComponent(elementId).textContent = count;
 }
 
+//Updates total number of chores completed
 function updateTotal() {
   total =
     parseInt(pottyCounter.textContent) +
@@ -39,48 +46,46 @@ function updateTotal() {
   totalCounter.textContent = total;
 }
 
-//Starts Timer
-
-function startTimer() {
-  if (paused) {
-    paused = false;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = totalSeconds % 60;
-  
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-    timerSpan.textContent = `${minutes}:${seconds}`;
-    timer = setInterval(countdown, 1000);
-  }
-}
-
-//Pauses Timer
-
-function pauseTimer() {
-    paused = true;
-    if (timer) {
-        clearInterval(timer);
-    }
-}
-
-
-//Resets Timer
-
-function resetTimer() {
-    paused = true;
-    clearInterval(timer);
-    initialMinutes = 20;
-    totalSeconds = initialMinutes * 60;
-    timerSpan.textContent = `${initialMinutes}:00`;
-}
-
-function countdown() {
+// Sets initial time calculations
+function setTimeCalculations() {
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = totalSeconds % 60;
 
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
   timerSpan.textContent = `${minutes}:${seconds}`;
+}
+
+
+//Starts Timer
+function startTimer() {
+  if (paused) {
+    paused = false;
+    setTimeCalculations();
+    timer = setInterval(countdown, 1000);
+  }
+}
+
+//Pauses Timer
+function pauseTimer() {
+  paused = true;
+  if (timer) {
+    clearInterval(timer);
+  }
+}
+
+//Resets Timer
+function resetTimer() {
+  paused = true;
+  clearInterval(timer);
+  totalSeconds = initialMinutes * 60;
+  timerSpan.textContent = `${initialMinutes}:00`;
+}
+
+
+//Starts Countdown
+function countdown() {
+  setTimeCalculations();
   totalSeconds--;
 
   if (totalSeconds === 0) {
